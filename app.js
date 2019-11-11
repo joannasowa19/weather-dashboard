2ba21161d85b06f587d65658340baa812f13c200
Joanna Sowa <joanna.sowa19@gmail.com>
2019-11-11 10:15:57 -0600

    more updates

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
-$(document).ready(function() {
-  // database //
-  // raw weather object from API //
-  var cityWeather =
-    "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"; // for now example is for london b/c of website //
-  let apiKey = "6f3f1bb1f2450a9219117bb18f842d80";
+// database //
+// raw weather object from API //
+let city = "";
+let storedData = JSON.parse(localStorage.getItem("saved"));
+let cities = storedData || [];
 
-  // variables //
-  // can create an empty (parsed) weather object //
+// variables //
+// can create an empty (parsed) weather object //
 
-  // utility functions //
-  // get raw data //
-  // parse raw data //
-  // render parsed data //
-
-  // event functions //
-  // search button click //
-  // have city name //
-  // send city name to an open weather API //
-  // set the weather info to the object returned (see raw data set up) //
-
-  // init //
-  // check local storage for history of cities and render them - this takes care of the init //
+// utility functions //
+function searchCity(city) {
+  let apiKey =
+    "https:api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=6f3f1bb1f2450a9219117bb18f842d80";
   $.ajax({
-    url: cityWeather,
+    url: apiKey,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
-    /* parse data */
+    let cityName = response.name;
+    let temp = response.main.temp;
+    let humid = response.main.humidity;
+    let wind = response.wind.speed;
+    $("#cityName").html(cityName);
+    $("#temperature").html(`Temperature : ${temp} F`);
+    $("#humidity").html(`Humidity : ${humid} %`);
+    $("#windSpeed").html(`Wind Speed : ${wind}mph`);
   });
-});
+}
+
+function forecast(city) {
+  var queryUrl =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&appid=6f3f1bb1f2450a9219117bb18f842d80";
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    displayForecast(response);
+  });
+}
+// get raw data //
+// parse raw data //
+// render parsed data //
+
+// event functions //
+// search button click //
+// have city name //
+// send city name to an open weather API //
+// set the weather info to the object returned (see raw data set up) //
+
+// init //
+// check local storage for history of cities and render them - this takes care of the init //
